9e64f7ecab266deee5c99aab83f520055aa3edce
babita1523 <babitasr1523@gmail.com>
2023-09-27 22:34:10 +0530

    adding spinner

--- src/component/dashboard/index.js
+++ src/component/dashboard/index.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import Spinner from 'react-bootstrap/Spinner';
 import BarGraph from './barGraph';
 import Card from './Card';
 
@@ -20,15 +21,20 @@ const Dashboard = () => {
 	const [productType, setProductType] = useState([])
 	const [dashboardCount, setDashboardCount] = useState({ customerCount: 0, loanAmount: 0, totalProductIn: 0, totalProductOut: 0 })
 	const [rooms, setRoom] = useState([])
+	const [loading, setLoading] = useState(false)
 
 	const detail = parseJwt(sessionStorage.getItem('token'))
 	sessionStorage.setItem('storeId', detail["storeId "])
 	const storeId = detail['storeId '].trim()
 
 	useEffect(() => {
+		setLoading(true)
 		getDashboardCountApi(year)
-			.then(res => res && setDashboardCount(res))
-			.catch(err => console.log(err))
+			.then(res => {
+				res && setDashboardCount(res)
+				setLoading(false)
+			})
+			.catch(err => setLoading(false))
 	}, [year])
 
 	const handleSelect = (value) => {
@@ -71,16 +77,25 @@ const Dashboard = () => {
 	}
 	return (
 		<>
-			<Card
-				dashboardCount={dashboardCount}
-				handleSelect={handleSelect} />
-			<ProductFilter
-				roomsArr={rooms}
-				product={state}
-				productType={productType}
-				handleChange={handleChange}
-			/>
-			<BarGraph />
+			{loading ?
+				<div className='main-content'>
+					<div className='card cardSpace'>
+						<div className='card-body'>
+							<div className='flex-block ht-75' style={{ justifyContent: 'center' }}><Spinner animation="border" />
+							</div>
+						</div>
+					</div>
+				</div>
+				: <><Card
+					dashboardCount={dashboardCount}
+					handleSelect={handleSelect} />
+					<ProductFilter
+						roomsArr={rooms}
+						product={state}
+						productType={productType}
+						handleChange={handleChange}
+					/>
+					<BarGraph /></>}
 		</>
 
 	)
